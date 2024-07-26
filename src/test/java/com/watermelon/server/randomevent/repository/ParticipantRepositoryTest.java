df21888eaa1e7a1e77da99d4fc65fdfcf908bd2a
ysndy <ysndy@naver.com>
2024-07-26 20:49:28 +0900

    feat: uid 로 참여자 조회하는 로직 추가

--- src/test/java/com/watermelon/server/randomevent/repository/ParticipantRepositoryTest.java
+++ src/test/java/com/watermelon/server/randomevent/repository/ParticipantRepositoryTest.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
+import java.util.Optional;
 
+import static com.watermelon.server.Constants.TEST_UID;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -40,5 +42,25 @@ class ParticipantRepositoryTest {
         //then
         assertThat(participants).hasSize(1);
 
+    }
+
+    @Test
+    @DisplayName("uid 가 일치하는 참여자를 반환한다.")
+    void findByUid() {
+
+        //given
+        Optional<Participant> expected = Optional.of(
+                Participant.builder()
+                .uid(TEST_UID)
+                .build()
+        );
+
+        participantRepository.save(expected.get());
+
+        //when
+        Optional<Participant> actual = participantRepository.findByUid(TEST_UID);
+
+        //then
+        assertThat(actual).isEqualTo(expected);
     }
 }
