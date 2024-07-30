5f854f1a09d46830a9c67f3aca65adeab2b3b98d
ysndy <ysndy@naver.com>
2024-07-30 11:59:56 +0900

    feat[api-server]: 응모자-파츠 서비스) 파츠 추가 기능 구현 및 테스트

--- src/main/java/com/watermelon/server/randomevent/parts/domain/Parts.java
+++ src/main/java/com/watermelon/server/randomevent/parts/domain/Parts.java
@@ -4,12 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Parts {
 
     @Id @GeneratedValue
---
+++ src/main/java/com/watermelon/server/randomevent/parts/repository/LotteryApplierPartsRepository.java
@@ -0,0 +1,12 @@
+package com.watermelon.server.randomevent.parts.repository;
+
+import com.watermelon.server.randomevent.parts.domain.LotteryApplierParts;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface LotteryApplierPartsRepository extends JpaRepository<LotteryApplierParts, Long> {
+
+    List<LotteryApplierParts> findLotteryApplierPartsByLotteryApplierId(Long lotteryApplierId);
+
+}
