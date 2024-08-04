bb7a2108024b0447236b0c1f0c197445cc1a69f0
ysndy <ysndy@naver.com>
2024-08-04 12:58:42 +0900

    refactor[api-server]: 로그인 인터셉터 목객체 추가

--- src/test/java/com/watermelon/server/MockLoginInterceptorConfig.java
+++ src/test/java/com/watermelon/server/MockLoginInterceptorConfig.java
@@ -1,6 +1,8 @@
 package com.watermelon.server;
 
 import com.watermelon.server.randomevent.auth.service.TokenVerifier;
+import com.watermelon.server.randomevent.link.service.LinkService;
+import com.watermelon.server.randomevent.service.LotteryService;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +18,16 @@ public class MockLoginInterceptorConfig {
         TokenVerifier tokenVerifier = Mockito.mock(TokenVerifier.class);
         Mockito.when(tokenVerifier.verify(TEST_TOKEN)).thenReturn(TEST_UID);
         return tokenVerifier;
+    }
+
+    @Bean
+    public LinkService mockLinkService() {
+        return Mockito.mock(LinkService.class);
+    }
+
+    @Bean
+    public LotteryService mockLotteryService() {
+        return Mockito.mock(LotteryService.class);
     }
 
 }
