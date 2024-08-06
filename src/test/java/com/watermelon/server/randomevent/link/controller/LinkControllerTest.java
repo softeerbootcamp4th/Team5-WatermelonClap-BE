99d7ddccf0cc6ab591b7f46873b4cbdac955c541
ysndy <ysndy@naver.com>
2024-08-06 15:21:48 +0900

    refactor[api-server]: 작업 내용 병합

--- src/test/java/com/watermelon/server/randomevent/link/controller/LinkControllerTest.java
+++ src/test/java/com/watermelon/server/randomevent/link/controller/LinkControllerTest.java
@@ -8,7 +8,9 @@ import com.watermelon.server.annotations.ControllerTest;
 import com.watermelon.server.randomevent.controller.LotteryController;
 import com.watermelon.server.randomevent.link.dto.MyLinkDto;
 import com.watermelon.server.randomevent.link.service.LinkService;
+import com.watermelon.server.randomevent.link.utils.LinkUtils;
 import com.watermelon.server.randomevent.service.LotteryService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,20 +19,25 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
 import static com.watermelon.server.Constants.*;
 import static com.watermelon.server.common.constants.PathConstants.MY_LINK;
+import static com.watermelon.server.common.constants.PathConstants.SHORTED_LINK;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 
 @ControllerTest
 @WebMvcTest(LinkController.class)
 class LinkControllerTest {
 
+    //TODO 추상클래스 상속 구조로 변경
     @Autowired
     private MockMvc mockMvc;
 
@@ -56,5 +63,18 @@ class LinkControllerTest {
                 .andExpect(content().json(objectMapper.writeValueAsString(expected)))
                 .andDo(document(DocumentConstants.MY_LINK));
 
+    }
+
+    @Test
+    @DisplayName("링크의 URI 가 포함된 주소로 리디렉션한다.")
+    void redirect() throws Exception {
+
+        //given
+        Mockito.when(linkService.getUrl(TEST_SHORTED_URI)).thenReturn(TEST_URI);
+
+        //when & then
+        this.mockMvc.perform(get(SHORTED_LINK, TEST_SHORTED_URI))
+                .andExpect(status().isFound())
+                .andExpect(header().string(HEADER_NAME_LOCATION, LinkUtils.makeUrl(TEST_URI)));
     }
 }
