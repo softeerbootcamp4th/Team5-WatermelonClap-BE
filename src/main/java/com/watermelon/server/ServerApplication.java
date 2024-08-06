a6b71ae15c7a5d5ce624cf09fbc619b261f8871f
starwook <star.wookk@gmail.com>
2024-08-06 14:21:41 +0900

    refactor[api-server]: Test시에 Local Profile을 의존하도록 수종

--- src/main/java/com/watermelon/server/ServerApplication.java
+++ src/main/java/com/watermelon/server/ServerApplication.java
@@ -8,17 +8,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableJpaAuditing
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
-	}
-
-	@Profile("!test")
-	@EnableJpaAuditing
-	@Configuration
-	static class JpaAuditingConfiguration {
 	}
 
 }
