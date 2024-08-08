5b70bb97a18423406ac1cb3eb40b968a6e7a6d0e
ysndy <ysndy@naver.com>
2024-08-08 19:14:30 +0900

    refactor[api-server]: 통합 테스트 클래스 분리하도록 수정

---
+++ src/test/java/com/watermelon/server/event/lottery/link/LinkIntegrationTest.java
@@ -0,0 +1,22 @@
+package com.watermelon.server.event.lottery.link;
+
+import com.watermelon.server.BaseIntegrationTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("[통합] 링크 통합 테스트")
+public class LinkIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    @DisplayName("[예정] 공유된 링크를 통해 이벤트 페이지로 이동해 가입한 사람이 있다면 링크의 유저에게 뽑기권을 1개 지급한다.")
+    void test2() {
+
+    }
+
+    @Test
+    @DisplayName("[예정] 공유한 링크를 통해 컬렉션 페이지에 방문한 사람에게는 장착된 파츠만 전달한다.")
+    void test3() {
+
+    }
+
+}
--- src/test/java/com/watermelon/server/event/lottery/parts/PartsIntegrationTest.java
+++ src/test/java/com/watermelon/server/event/lottery/parts/PartsIntegrationTest.java
@@ -1,4 +1,4 @@
-package com.watermelon.server.event.lottery;
+package com.watermelon.server.event.lottery.parts;
 
 import com.watermelon.server.BaseIntegrationTest;
 import com.watermelon.server.event.lottery.auth.service.TestTokenVerifier;
@@ -10,7 +10,6 @@ import com.watermelon.server.event.lottery.parts.repository.LotteryApplierPartsR
 import com.watermelon.server.event.lottery.parts.repository.PartsRepository;
 import com.watermelon.server.event.lottery.repository.LotteryApplierRepository;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +18,8 @@ import static com.watermelon.server.Constants.*;
 import static com.watermelon.server.common.constants.PathConstants.PARTS;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-@DisplayName("[통합] 추첨 통합 테스트")
-public class LotteryIntegrationTest extends BaseIntegrationTest {
+@DisplayName("[통합] 파츠 통합 테스트")
+public class PartsIntegrationTest extends BaseIntegrationTest {
 
     @Autowired
     private LotteryApplierRepository lotteryApplierRepository;
@@ -28,21 +27,35 @@ public class LotteryIntegrationTest extends BaseIntegrationTest {
     @Autowired
     private PartsRepository partsRepository;
 
-    @BeforeEach
-    void setUp() {
-        partsRepository.saveAll(Parts.createAllParts());
-    }
+    @Autowired
+    private LotteryApplierPartsRepository lotteryApplierPartsRepository;
 
     @Test
-    @DisplayName("내 아반떼 N 뽑기 이벤트에 최초 참여시 자동으로 응모된다.")
-    void applySuccessTest() throws Exception {
+    @DisplayName("각 카테고리의 파츠를 1개 이상 뽑으면 '나만의 아반떼 N 미니어처'를 받을 수 있는 추첨권을 부여한다.")
+    void partsLotteryPolicyTest() throws Exception {
+
+        //given
+        LotteryApplier lotteryApplier = saveTestLotteryApplier();
+        for(PartsCategory partsCategory:PartsCategory.values()){
+            Parts parts = partsRepository.save(Parts.createTestCategoryParts(partsCategory));
+            lotteryApplierPartsRepository.save(
+                    LotteryApplierParts.createApplierParts(true, lotteryApplier, parts)
+            );
+        }
 
         //when
         postPartsLotteryRequestTestUser();
+        boolean expected = findTestLotteryApplier().isPartsApplier();
 
         //then
-        LotteryApplier lotteryApplier = findTestLotteryApplier();
-        Assertions.assertThat(lotteryApplier.isLotteryApplier()).isTrue();
+        Assertions.assertThat(expected).isTrue();
+
+    }
+
+    private LotteryApplier saveTestLotteryApplier() {
+        LotteryApplier lotteryApplier = LotteryApplier.createLotteryApplier(TestTokenVerifier.TEST_UID);
+        lotteryApplierRepository.save(lotteryApplier);
+        return lotteryApplier;
     }
 
     private LotteryApplier findTestLotteryApplier() {
