d54f89f0d8ec8e8c5018a570cdff35dcbba7c41c
ysndy <ysndy@naver.com>
2024-08-08 17:14:46 +0900

    feat[api-server]: 카테고리 기준 중복 제거한 파츠 수를 세는 메소드 추가

--- src/main/java/com/watermelon/server/event/lottery/parts/repository/PartsRepository.java
+++ src/main/java/com/watermelon/server/event/lottery/parts/repository/PartsRepository.java
@@ -2,7 +2,11 @@ package com.watermelon.server.event.lottery.parts.repository;
 
 import com.watermelon.server.event.lottery.parts.domain.Parts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PartsRepository extends JpaRepository<Parts, Long> {
+
+    @Query("select count(distinct parts.category) from Parts parts")
+    long countCategoryDistinct();
 
 }
