3eb0d139864eb21c396e11c4c330ff65992e26d8
ysndy <ysndy@naver.com>
2024-08-08 17:10:37 +0900

    feat[api-server]: 기본 파츠 목록 생성 메소드 추가

--- src/main/java/com/watermelon/server/event/lottery/parts/domain/Parts.java
+++ src/main/java/com/watermelon/server/event/lottery/parts/domain/Parts.java
@@ -30,4 +30,111 @@ public class Parts {
     @OneToMany(mappedBy = "parts")
     private List<LotteryApplierParts> lotteryApplierParts;
 
+    public static Parts createTestCategoryParts(PartsCategory category){
+        return Parts.builder()
+                .category(category)
+                .build();
+    }
+
+    public static List<Parts> createAllParts(){
+        return List.of(
+                Parts.builder()
+                        .name("홀로그램")
+                        .category(PartsCategory.COLOR)
+                        .description("홀로그램입니다.")
+                        .imgSrc("www.s3.com/hg")
+                        .build(),
+                Parts.builder()
+                        .name("레이싱")
+                        .category(PartsCategory.COLOR)
+                        .description("레이싱입니다.")
+                        .imgSrc("www.s3.com/rc")
+                        .build(),
+                Parts.builder()
+                        .name("퍼플")
+                        .category(PartsCategory.COLOR)
+                        .description("퍼플입니다.")
+                        .imgSrc("www.s3.com/rc")
+                        .build(),
+                Parts.builder()
+                        .name("형광 노랑")
+                        .category(PartsCategory.COLOR)
+                        .description("형광 노랑입니다.")
+                        .imgSrc("www.s3.com/rc")
+                        .build(),
+                Parts.builder()
+                        .name("피치")
+                        .category(PartsCategory.COLOR)
+                        .description("피치입니다.")
+                        .imgSrc("www.s3.com/rc")
+                        .build(),
+                Parts.builder()
+                        .name("연두")
+                        .category(PartsCategory.COLOR)
+                        .description("연두입니다.")
+                        .imgSrc("www.s3.com/rc")
+                        .build(),
+                Parts.builder()
+                        .name("스플래시")
+                        .category(PartsCategory.COLOR)
+                        .description("스플래시입니다.")
+                        .imgSrc("www.s3.com/rc")
+                        .build(),
+                Parts.builder()
+                        .name("카본 리어 스포일러")
+                        .category(PartsCategory.REAR)
+                        .description("카본 리어 스포일러입니다.")
+                        .imgSrc("www.s3.com/rc")
+                        .build(),
+                Parts.builder()
+                        .name("CFRP 리어 스포일러")
+                        .category(PartsCategory.REAR)
+                        .description("CFRP 리어 스포일러입니다.")
+                        .imgSrc("www.s3.com/rc")
+                        .build(),
+                Parts.builder()
+                        .name("ECO")
+                        .category(PartsCategory.DRIVE_MODE)
+                        .description("ECO입니다.")
+                        .imgSrc("www.s3.com/rc")
+                        .build(),
+                Parts.builder()
+                        .name("NORMAL")
+                        .category(PartsCategory.DRIVE_MODE)
+                        .description("NORMAL입니다.")
+                        .imgSrc("www.s3.com/rc")
+                        .build(),
+                Parts.builder()
+                        .name("SPORT")
+                        .category(PartsCategory.DRIVE_MODE)
+                        .description("SPORT입니다.")
+                        .imgSrc("www.s3.com/rc")
+                        .build(),
+                Parts.builder()
+                        .name("N 모드")
+                        .category(PartsCategory.DRIVE_MODE)
+                        .description("N 모드입니다.")
+                        .imgSrc("www.s3.com/rc")
+                        .build(),
+                Parts.builder()
+                        .name("단조 휠 화이트")
+                        .category(PartsCategory.WHEEL)
+                        .description("단조 휠 화이트입니다.")
+                        .imgSrc("www.s3.com/rc")
+                        .build(),
+                Parts.builder()
+                        .name("실버")
+                        .category(PartsCategory.WHEEL)
+                        .description("실버입니다.")
+                        .imgSrc("www.s3.com/rc")
+                        .build(),
+                Parts.builder()
+                        .name("매트 블랙")
+                        .category(PartsCategory.WHEEL)
+                        .description("매트 블랙입니다.")
+                        .imgSrc("www.s3.com/rc")
+                        .build()
+        );
+    }
+
 }
