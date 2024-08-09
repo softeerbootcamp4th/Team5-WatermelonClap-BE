7fcbf5f84b34acc57b967d6e8f932bfef6d8c03b
starwook <star.wookk@gmail.com>
2024-08-09 11:42:09 +0900

    feat[api-server]: 어드민 선착순 이벤트 API 명세서 초기 구현

--- src/test/java/com/watermelon/server/admin/controller/AdminOrderEventControllerTest.java
+++ src/test/java/com/watermelon/server/admin/controller/AdminOrderEventControllerTest.java
@@ -1,20 +1,143 @@
 package com.watermelon.server.admin.controller;
 
+import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.watermelon.server.ControllerTest;
+import com.watermelon.server.admin.exception.S3ImageFormatException;
+import com.watermelon.server.admin.service.AdminOrderEventService;
+import com.watermelon.server.event.order.controller.OrderEventController;
+import com.watermelon.server.event.order.domain.OrderEvent;
+import com.watermelon.server.event.order.domain.OrderEventStatus;
+import com.watermelon.server.event.order.dto.request.RequestOrderEventDto;
+import com.watermelon.server.event.order.dto.request.RequestOrderRewardDto;
+import com.watermelon.server.event.order.dto.request.RequestQuizDto;
+import com.watermelon.server.event.order.dto.response.ResponseOrderEventDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
-class AdminOrderEventControllerTest {
+import static com.watermelon.server.Constants.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AdminOrderEventControllerTest.class)
+class AdminOrderEventControllerTest extends ControllerTest {
 
 
-    @DisplayName("선착순 이벤트 생성")
-    @Test
-    void makeOrderEventWithOutImageWithOutImage() {
+    @MockBean
+    private AdminOrderEventService adminOrderEventService;
+
+
+    private ResponseOrderEventDto openOrderEventResponse;
+    private ResponseOrderEventDto soonOpenResponse;
+    private ResponseOrderEventDto unOpenResponse;
+    private OrderEvent soonOpenOrderEvent;
+    private OrderEvent openOrderEvent;
+    private OrderEvent unOpenOrderEvent;
+    private List<OrderEvent> orderEvents = new ArrayList<>();
+    private List<ResponseOrderEventDto> responseOrderEventDtos = new ArrayList<>();
+    @BeforeEach
+    void setUp(){
+        openOrderEvent = makeOrderEventIdWithDoc(1L);
+        soonOpenOrderEvent = makeOrderEventIdWithDoc(2L);
+        unOpenOrderEvent = makeOrderEventIdWithDoc(3L);
+        openOrderEvent.setOrderEventStatus(OrderEventStatus.OPEN);
+
+        orderEvents.add(openOrderEvent);
+        orderEvents.add(soonOpenOrderEvent);
+        orderEvents.add(unOpenOrderEvent);
+
+        openOrderEventResponse = ResponseOrderEventDto.forAdmin(openOrderEvent);
+        soonOpenResponse = ResponseOrderEventDto.forAdmin(soonOpenOrderEvent);
+        unOpenResponse = ResponseOrderEventDto.forAdmin(unOpenOrderEvent);
+
+        responseOrderEventDtos.add(openOrderEventResponse);
+        responseOrderEventDtos.add(soonOpenResponse);
+        responseOrderEventDtos.add(unOpenResponse);
+
+    }
+    private static OrderEvent makeOrderEventIdWithDoc(Long id) {
+        return OrderEvent.makeOrderEventWithInputIdForDocumentation(
+                RequestOrderEventDto.makeForTestOpened(
+                        RequestQuizDto.makeForTest(),
+                        RequestOrderRewardDto.makeForTest()
+                ), id
+        );
     }
 
-    @DisplayName("퀴즈 정답 존재 확인")
     @Test
-    void getOrderEventForAdmin() {
+    @DisplayName("[DOC] 어드민 선착순 이벤트 목록을 가져온다")
+    void getOrderEvents() throws Exception {
+        final String PATH = "/admin/event/order";
+        final String DOCUMENT_NAME ="admin/event/order";
+        Mockito.when(adminOrderEventService.getOrderEventsForAdmin()).thenReturn(responseOrderEventDtos);
+
+        mockMvc.perform(RestDocumentationRequestBuilders.get(PATH)
+                        .header(HEADER_NAME_AUTHORIZATION, HEADER_VALUE_BEARER + " " + TEST_TOKEN))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(responseOrderEventDtos)))
+                .andDo(print())
+                .andDo(MockMvcRestDocumentationWrapper.document(DOCUMENT_NAME,
+                        resourceSnippet("선착순 이벤트 목록 조회")));
+    }
+
+
+//    @Test
+//    @DisplayName("[DOC] 선착순 이벤트를 생성한다")
+//    void makeOrderEvent() throws Exception {
+//        final String PATH = "/admin/event/order";
+//        final String DOCUMENT_NAME ="admin/event/order";
+//        Mockito.when(adminOrderEventService.makeOrderEvent(any(),any(),any())).thenReturn(openOrderEventResponse);
+//
+//        MockMultipartFile rewardImage = new MockMultipartFile("rewardImage", "tooth.png", "multipart/form-data", "uploadFile".getBytes(StandardCharsets.UTF_8));
+//        MockMultipartFile quizImage  = new MockMultipartFile("quizImage", "tooth.png", "multipart/form-data", "uploadFile".getBytes(StandardCharsets.UTF_8));
+//
+//        mockMvc.perform(
+//                        MockMvcRequestBuilders
+//                                .multipart(HttpMethod.POST,PATH)
+//                                .file(rewardImage)
+//                                .file(quizImage)
+//                                .accept(MediaType.APPLICATION_JSON)
+//                                .contentType(MediaType.MULTIPART_FORM_DATA)
+//                )
+//                .andExpect(status().isOk())
+//                .andDo(print());
+//
+//    }
+
+
+    //    RestDocumentationRequestBuilders.post(PATH)
+//            .contentType(MediaType.APPLICATION_JSON)
+//                        .content(objectMapper.writeValueAsString(
+//            RequestOrderEventDto.makeForTestOpened(
+//            RequestQuizDto.makeForTest(),RequestOrderRewardDto.makeForTest()
+//                                )
+//                                        ))
+    @Test
+    void testGetOrderEventForAdmin() {
+    }
+
+    @Test
+    void getOrderEventWinnersForAdmin() {
+    }
+
+    @Test
+    void handlePhoneNumberNotExistException() {
     }
 }
