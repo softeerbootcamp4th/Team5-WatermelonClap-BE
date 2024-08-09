28f817af5c870b595007f15123c6a2442c9bf3ad
ysndy <ysndy@naver.com>
2024-08-09 23:08:30 +0900

    refactor[api-server]: Origin, Methods 수정

--- src/main/java/com/watermelon/server/common/filter/CorsFilter.java
+++ src/main/java/com/watermelon/server/common/filter/CorsFilter.java
@@ -23,9 +23,9 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods","*");
+        response.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, PATCH, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Authorization");
