57c281db300e57d40177a6db483affd2882758fc
ysndy <ysndy@naver.com>
2024-08-09 22:58:34 +0900

    refactor[api-server]: Cors 필터로 변경

--- src/main/java/com/watermelon/server/common/config/WebConfig.java
+++ src/main/java/com/watermelon/server/common/config/WebConfig.java
@@ -44,14 +44,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/admin/**");
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOriginPatterns("http://localhost:5173")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+//                .allowedHeaders("*")
+//                .maxAge(3600);
+//    }
 
 }
